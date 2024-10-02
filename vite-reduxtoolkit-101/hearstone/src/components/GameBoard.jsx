9f7a10d510c6197d97bf90d59bf3b550c4b1d268
Engin Karatas <enginkaratas99@gmail.com>
2024-10-02 23:55:24 +0300

    remove unnecessary classes

--- vite-reduxtoolkit-101/hearstone/src/components/GameBoard.jsx
+++ vite-reduxtoolkit-101/hearstone/src/components/GameBoard.jsx
@@ -1,6 +1,6 @@
 export default function GameBoard() {
   return (
-    <div className="bg bg-game-board bg-cover bg-center">
+    <div className="">
         <img src="../public/hearthstone-board-2.png" height="100" alt="game board" className="board" />
     </div>
   )
