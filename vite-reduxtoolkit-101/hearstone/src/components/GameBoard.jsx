4013f2bcad27df61b38b6bdd32b337ab64c2f874
Engin Karatas <enginkaratas99@gmail.com>
2024-10-02 23:58:24 +0300

    change border structer

--- vite-reduxtoolkit-101/hearstone/src/components/GameBoard.jsx
+++ vite-reduxtoolkit-101/hearstone/src/components/GameBoard.jsx
@@ -1,8 +1,11 @@
 export default function GameBoard() {
   return (
-    <div className="">
-        <img src="../public/hearthstone-board-2.png" height="100" alt="game board" className="board" />
+    <div className="board">
+      <img
+ 
+        height="100"
+        alt="game board"
+      />
     </div>
-  )
+  );
 }
-
