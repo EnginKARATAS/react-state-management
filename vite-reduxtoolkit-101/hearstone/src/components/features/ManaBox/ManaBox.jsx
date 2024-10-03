a4f9e7534b571c782eebfc14cf85f6c1c07baaa2
Engin Karatas <enginkaratas99@gmail.com>
2024-10-03 21:54:21 +0300

    update mana box last touches

--- vite-reduxtoolkit-101/hearstone/src/components/features/ManaBox/ManaBox.jsx
+++ vite-reduxtoolkit-101/hearstone/src/components/features/ManaBox/ManaBox.jsx
@@ -6,7 +6,7 @@ export default function ManaBox() {
       <div className="mana-counter flex justify-center items-center">
         <p className="text-sky-400 px-2">0/10</p>
       </div>
-      <div className="mana-cyrstals flex flex-row gap-1">
+      <div className="mana-cyrstals flex flex-row gap-1 justify-center items-center">
         <img src="/public/mana-bg.png" alt="mana-crystal" className="mana-cyrstal" />
         <img src="/public/mana-bg.png" alt="mana-crystal" className="mana-cyrstal" />
         <img src="/public/mana-bg.png" alt="mana-crystal" className="mana-cyrstal" />
