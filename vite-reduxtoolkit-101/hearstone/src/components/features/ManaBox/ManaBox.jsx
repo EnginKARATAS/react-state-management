b5a755a728e9c471683d3dc40b5f0ce76e363c7a
Engin Karatas <enginkaratas99@gmail.com>
2024-10-03 21:23:01 +0300

    tailwind css configuration base

--- vite-reduxtoolkit-101/hearstone/src/components/features/ManaBox/ManaBox.jsx
+++ vite-reduxtoolkit-101/hearstone/src/components/features/ManaBox/ManaBox.jsx
@@ -2,14 +2,13 @@ import "./ManaBox.css";
 
 export default function ManaBox() {
   return (
-    <div className="mana-container">
-      <div className="mana-counter">
-         <p className="text-white">0/10</p>
-        </div>
-        <div className="mana-crystals">
-         <img src="../../../assets/mana-bg.png" alt="mana-crystal" />
-        </div>
-      
+    <div className="mana-container flex flex-row">
+        <p className="text-sky-400">0/10</p>
+        <img
+          src="/public/mana-bg.png"
+          width="50"
+          alt="mana-crystal"
+        />
     </div>
   );
 }
