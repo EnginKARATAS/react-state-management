33d013e1dc808487eb9aa0b8042da6c1b97b7f5f
Engin Karatas <enginkaratas99@gmail.com>
2024-10-03 20:55:02 +0300

    add mana crystal

--- vite-reduxtoolkit-101/hearstone/src/components/features/ManaBox/ManaBox.jsx
+++ vite-reduxtoolkit-101/hearstone/src/components/features/ManaBox/ManaBox.jsx
@@ -2,8 +2,14 @@ import "./ManaBox.css";
 
 export default function ManaBox() {
   return (
-    <div className="mana-box">
-      <p className="text-white">test</p>
+    <div className="mana-container">
+      <div className="mana-counter">
+         <p className="text-white">0/10</p>
+        </div>
+        <div className="mana-crystals">
+         <img src="../../../assets/mana-bg.png" alt="mana-crystal" />
+        </div>
+      
     </div>
   );
 }
