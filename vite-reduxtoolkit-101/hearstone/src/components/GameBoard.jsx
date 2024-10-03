c68249fd77fb1eb6abc076d695cfb0de3566e3df
Engin Karatas <enginkaratas99@gmail.com>
2024-10-03 23:13:46 +0300

    create polygon button🐦‍🔥

--- vite-reduxtoolkit-101/hearstone/src/components/GameBoard.jsx
+++ vite-reduxtoolkit-101/hearstone/src/components/GameBoard.jsx
@@ -1,10 +1,11 @@
 import ManaBox from "./features/ManaBox/ManaBox";
+import EndTurnButton from "./features/EndTurnButton/EndTurnButton";
   
 export default function GameBoard() {
   return (
     <div className="board">
       <ManaBox />
-    
+      <EndTurnButton />
     </div>
   );
 }
---
+++ vite-reduxtoolkit-101/hearstone/src/components/features/EndTurnButton/EndTurnButton.jsx
@@ -0,0 +1,9 @@
+import "./EndTurnButton.css";
+
+export default function EndTurnButton() {
+    return (
+        <div className="end-turn-button m-3 bg-yellow-500 rounded-lg flex justify-center items-center">
+            <button>End Turn</button>
+        </div>
+    );
+}
