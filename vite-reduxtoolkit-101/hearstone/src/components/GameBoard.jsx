e9448acb82424b3a271ab0081c933cbc09e8062c
Engin Karatas <enginkaratas99@gmail.com>
2024-10-13 23:37:58 +0300

    create simple loading screen

--- vite-reduxtoolkit-101/hearstone/src/components/GameBoard.jsx
+++ vite-reduxtoolkit-101/hearstone/src/components/GameBoard.jsx
@@ -3,7 +3,25 @@ import FishOrnament from "./features/UI/FishOrnament";
 import YourTurn from "./features/UI/YourTurn/YourTurn";
 import ClientProfile from "./features/UI/Profile/ClientProfile/ClientProfile";
 import EnemyProfile from "./features/UI/Profile/EnemyProfile/EnemyProfile";
+import { useEffect, useState } from 'react';
+import LoadingScreen from './LoadingScreen';
+
 export default function GameBoard() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadCards = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      setLoading(false);
+    };
+
+    loadCards();
+  }, []);
+
+  if (loading) {
+    return <LoadingScreen />;
+  }
+
   return (
     <div className="board ">
       <FishOrnament />
---
+++ vite-reduxtoolkit-101/hearstone/src/components/LoadingScreen.jsx
@@ -0,0 +1,9 @@
+import "./LoadingScreen.css";  
+
+export default function LoadingScreen() {
+  return (
+    <div className="loading-screen fixed flex justify-center items-center ">
+      <h1>Loading...</h1>
+    </div>
+  );
+};
