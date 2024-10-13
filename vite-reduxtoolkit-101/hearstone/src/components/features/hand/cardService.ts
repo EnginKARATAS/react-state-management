9b450e5c333dbcea7c27f23fadd62f3e30fd5dc9
Engin Karatas <enginkaratas99@gmail.com>
2024-10-13 19:22:21 +0300

    add new property to base card service

--- vite-reduxtoolkit-101/hearstone/src/components/features/hand/cardService.ts
+++ vite-reduxtoolkit-101/hearstone/src/components/features/hand/cardService.ts
@@ -12,11 +12,11 @@ const clientCardBase: Card[] = [
     cardType: "minion",
     cardCost: 1,
     cardImageName: "dictator",
-    isPlayed: false,
     cardAttack: 4,
     cardHealth: 2,
     cardPosition: { x: 0, y: 0, top: 0, size: 150, offset: 0 },
     deg: 0,
+    move: 0
   },
   {
     cardOwner: "player",
@@ -31,11 +31,11 @@ const clientCardBase: Card[] = [
     cardType: "minion",
     cardCost: 3,
     cardImageName: "guitar",
-    isPlayed: false,
     cardAttack: 2,
     cardHealth: 2,
     cardPosition: { x: 0, y: 0, top: 0, size: 150, offset: 0 },
     deg: 0,
+    move: 0
   },
   {
     cardOwner: "player",
@@ -50,11 +50,11 @@ const clientCardBase: Card[] = [
     cardType: "minion",
     cardCost: 2,
     cardImageName: "soldier",
-    isPlayed: false,
     cardAttack: 1,
     cardHealth: 1,
     cardPosition: { x: 0, y: 0, top: 0, size: 150, offset: 0 },
     deg: 0,
+    move: 0
   },
   {
     cardOwner: "player",
@@ -69,11 +69,11 @@ const clientCardBase: Card[] = [
     cardType: "minion",
     cardCost: 2,
     cardImageName: "worrior",
-    isPlayed: false,
     cardAttack: 2,
     cardHealth: 9,
     cardPosition: { x: 0, y: 0, top: 0, size: 150, offset: 0 },
     deg: 0,
+    move: 0
   },
 ];
 
@@ -90,11 +90,11 @@ const enemyCardBase: Card[] = [
     cardType: "minion",
     cardCost: 1,
     cardImageName: "cix",
-    isPlayed: false,
     cardAttack: 3,
     cardHealth: 2,
     cardPosition: { x: 0, y: 0, top: 0, size: 150, offset: 0 },
     deg: 0,
+    move: 0
   },
   {
     cardOwner: "enemy",
@@ -108,11 +108,11 @@ const enemyCardBase: Card[] = [
     cardType: "minion",
     cardCost: 1,
     cardImageName: "hacker",
-    isPlayed: false,
     cardAttack: 3,
     cardHealth: 2,
     cardPosition: { x: 0, y: 0, top: 0, size: 150, offset: 0 },
     deg: 0,
+    move: 0
   },
   {
     cardOwner: "enemy",
@@ -126,11 +126,11 @@ const enemyCardBase: Card[] = [
     cardType: "minion",
     cardCost: 5,
     cardImageName: "sleeper",
-    isPlayed: false,
     cardAttack: 3,
     cardHealth: 8,
     cardPosition: { x: 0, y: 0, top: 0, size: 150, offset: 0 },
     deg: 0,
+    move: 0
   },
 ];
 
